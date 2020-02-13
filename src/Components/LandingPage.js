916e32f0636a3ebb688ad58e8c38291510a1e418
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-02-13 00:39:05 -0500

    sample backend connection test

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './styles/LandingPage.css'
 
 class LandingPage extends Component {
     constructor(){
         super();
+        this.state = {
+            msg: ""
+        }
     }
+
+    componentWillMount() {
+        axios.get('http://localhost:8080/ping?name=jackson')
+        .then(res => {
+            const msg = res.data;
+            this.setState({ msg })
+        })
+    }
+
     render() {
         return(
-            <h1>Landing Page</h1>
+            <div>
+                <h1>Landing Page</h1>
+                <p> {this.state.msg} </p>
+            </div>
         );
     }
 }
