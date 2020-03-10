4a8d2758f67a8783ae695900b4b8112cd468c147
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-03-10 02:57:05 -0400

    made landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { MDBInput, MDBBtn, MDBRow, MDBCol, MDBContainer } from "mdbreact";
 import axios from 'axios';
 import './styles/LandingPage.css'
 
@@ -10,20 +11,60 @@ class LandingPage extends Component {
         }
     }
 
-    componentWillMount() {
-        axios.get('http://localhost:8080/ping?name=jackson')
-        .then(res => {
-            const msg = res.data;
-            this.setState({ msg })
-        })
-    }
-
     render() {
         return(
-            <div>
-                <h1>Landing Page</h1>
-                <p> {this.state.msg} </p>
-            </div>
+            <MDBContainer id="landingpage">
+                <MDBRow id="landingpage-section-1">
+                    <MDBCol size="6">
+                        <div id="landingpage-header">
+                            <h1>Welcome to Rootly.</h1>
+                            <h3>Organize quick and casual events within the Waterloo Community</h3>
+                        </div>
+                    </MDBCol>
+                    <MDBCol size="6">
+                        <div id="email-signup">
+                            <h3>Your UW Email</h3>
+                            <MDBInput label="Email" />
+                            <MDBBtn id="email-signup-btn">Go!</MDBBtn>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+
+                <MDBRow>
+                    <MDBCol size="5">
+                        <h2 id="section-2-h2">In a new city? Explore it with fellow students!</h2>
+                    </MDBCol>
+                    <MDBCol size="7">
+                        <img src="section-2-image.jpg"></img>
+                    </MDBCol>
+                </MDBRow>
+
+                <MDBRow>
+                    <MDBCol size="10">
+                        <div className="event-card">
+                            <img src="event-icon.PNG"></img>
+                            <div id="event-card-description">
+                                <h4>Rockclimbing at Phil's</h4>
+                                <p>No one has made this event yet...It could be you!</p>
+                            </div>
+                            <MDBBtn id="make-event-btn">MAKE EVENT</MDBBtn>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+
+                <MDBRow>
+                    <MDBCol size="10">
+                        <div className="event-card">
+                            <img src="event-icon.PNG"></img>
+                            <div id="event-card-description">
+                                <h4>Bubble Tea at Icon</h4>
+                                <p>No one has made this event yet...It could be you!</p>
+                            </div>
+                            <MDBBtn id="make-event-btn">MAKE EVENT</MDBBtn>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+        </MDBContainer>
         );
     }
 }
