44bff7f74348d29c293715e2b5e1258912f11c87
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-03-14 01:39:23 -0400

    updated submit button

--- src/Components/SubmitButton.js
+++ src/Components/SubmitButton.js
@@ -1,5 +1,6 @@
 import { useHistory } from "react-router-dom";
 import React from 'react';
+import {MDBBtn} from "mdbreact";
 import axios from "axios";
 
 const SubmitButton = (props) => {
@@ -7,22 +8,37 @@ const SubmitButton = (props) => {
 
   function handleClick(e) {
     e.preventDefault();
-    axios.post('http://localhost:8080/signup', {
-        email: props.email,
-        password: props.password
-    })
+    let type = props.type;
+    let path = "";
+    if(type == "landingPage") {
+      path = `/users/exists?email=${props.email}`;
+    }
+    else if(type == "signup") {
+      path = '`/signup?email=${props.email}`';
+    }
+    // let { email, password } = props;
+    // console.log(email);
+    axios.get(`http://localhost:8080${path}`)
     .then((res) => {
         if (res.status == 200) {
             console.log(res);
-            history.push({pathname: '/login', isVerified: true});
+            let redirectPath = "";
+            if(type == "landingPage" && res.exists) {
+              redirectPath = "/signup";
+            }
+            else if (type == "signup") {
+              redirectPath = '/verify';
+            }
+            history.push({pathname: '/verify', email: "mickey@uwaterloo.ca"})
+            // history.push({pathname: '/login', isVerified: true});
         }
     })
   }
 
   return (
-    <button type="submit" onClick={handleClick}>
-      Submit
-    </button>
+    <MDBBtn id={props.id} type="submit" onClick={handleClick}>
+      Go!
+    </MDBBtn>
   );
 }
 
