167801272e4def14929221cad514e5f631cdda02
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-03-14 01:39:32 -0400

    created verifypage

---
+++ src/Components/VerifyPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles/VerifyPage.css'
+
+class VerifyPage extends Component {
+    constructor(){
+        super();
+    }
+    render() {
+        return(
+            <div id="verify-page">
+                <h1>Verification Sent To:</h1>
+                <p>{this.props.location.email}</p>
+                <h1>See you in a bit!</h1>
+            </div>
+        );
+    }
+}
+
+export default VerifyPage
