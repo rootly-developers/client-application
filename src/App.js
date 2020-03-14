11aa98081e35babfea97956f2e9b4c1183a0fe27
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-03-14 01:37:15 -0400

    added an id to bootstrap container

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     return(
       <div id="bootstrap-override">
-        <MDBContainer fluid>
+        <MDBContainer fluid id="app-container">
           <NavBar/>
         </MDBContainer>
       </div>
