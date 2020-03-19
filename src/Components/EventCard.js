f6e4216640846780110c5be739e509b03185cf9a
Jackson Zheng <j75zheng@edu.uwaterloo.ca>
2020-03-19 01:17:35 -0400

    updated event card to use grid system

--- src/Components/EventCard.js
+++ src/Components/EventCard.js
@@ -4,18 +4,39 @@ import './styles/EventCard.css'
 
 const EventCard = (props) => {
     let btnMsg = props.sample ? "MAKE EVENT":"JOIN";
+    let attendeesMsg = props.attendees ? (props.attendees + "/" + props.maxAttendees):""; 
     return (
-        <div className="event-card">
-            <img src="event-icon.PNG"></img>
-            <div className="event-card-right">
-                <div id="event-card-description">
+        <MDBRow className="event-card">
+            <MDBCol size="3">
+                <img src="event-icon.PNG"></img>
+            </MDBCol>
+
+            <MDBCol size="5" className="eventcard-content-col">
+                <div className="eventcard-content">
                     <h4>{props.title}</h4>
                     <p>{props.description}</p>
                 </div>
-                <MDBBtn id="make-event-btn">{btnMsg}</MDBBtn>
-            </div>
+            </MDBCol>
 
-        </div>
+            <MDBCol size="1" className="eventcard-attendees-col">
+                <p>{attendeesMsg}</p>
+            </MDBCol>
+
+            <MDBCol size="3" className="eventcard-btn-col">
+                <MDBBtn id="make-event-btn" className="eventcard-btn">{btnMsg}</MDBBtn>
+            </MDBCol>
+        </MDBRow>
+        // <div className="event-card">
+        //     <img src="event-icon.PNG"></img>
+        //     <div className="event-card-right">
+        //         <div id="event-card-description">
+        //             <h4>{props.title}</h4>
+        //             <p>{props.description}</p>
+        //         </div>
+        //         <p>{props.attendees}/{props.maxAttendees}</p>
+        //         <MDBBtn id="make-event-btn">{btnMsg}</MDBBtn>
+        //     </div>
+        // </div>
     );
   }
   
