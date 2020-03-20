f584abeb3551db4bd59a42e9639754163dc13630
thedarrch <dtcheung@edu.uwaterloo.ca>
2020-03-19 23:34:33 -0400

    remove breaks, add margins, for spacing,  add italics

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -21,7 +21,7 @@ class LandingPage extends Component {
                         <div id="landingpage-header">
                             <h1>Welcome to Rootly.</h1>
                             <h3>Organize quick and casual events within the Waterloo Community</h3>
-                            <h2>What does that look like?</h2>
+                            <h2 style={{fontStyle: "italic"}}>What does that look like?</h2>
                         </div>
                     </MDBCol>
                     <MDBCol size="6">
@@ -39,26 +39,22 @@ class LandingPage extends Component {
                     </MDBCol>
                 </MDBRow>
 
-                <MDBRow className="landingpage-section">
-                    <MDBCol size="5">
+                <MDBRow className="section-margin">
+                    <MDBCol size="7">
                         <h2 id="section-2-h2">In a new city? Explore it with fellow students!</h2>
                     </MDBCol>
-                    <MDBCol size="7">
-                        <img src="section-2-image.jpg" id="section-2-img" className="img-fluid"></img>
+                    <MDBCol size="5">
+                        <img src="section-2-image.jpg" className="img-fluid" id="section-2-img"></img>
                     </MDBCol>
                 </MDBRow>
 
-                <br></br>
-
-                <MDBRow>
+                <MDBRow className="section-margin">
                     <MDBCol size="12">
                         <EventCard title="Rockclimbing at Phil's" description="No one has made this event yet...It could be you!" sample={true}/>
                     </MDBCol>
                 </MDBRow>
 
-                <br></br>
-
-                <MDBRow>
+                <MDBRow className="section-margin">
                     <MDBCol size="7">
                         <img src="section-3-image.jpg" className="img-fluid" id="section-3-img"></img>
                     </MDBCol>
@@ -67,28 +63,22 @@ class LandingPage extends Component {
                     </MDBCol>
                 </MDBRow>
 
-                <br></br>
-
                 <MDBRow>
                     <MDBCol size="12">
                         <EventCard title="Hunt a Goose with Matlab" description="No one has made this event yet...It could be you!" sample={true}/>
                     </MDBCol>
                 </MDBRow>
 
-                <br></br>
-
-                <MDBRow>
-                    <MDBCol size="5">
+                <MDBRow className="section-margin">
+                    <MDBCol size="7">
                         <h2 id="section-4-h2">Want to throw a board game night? Invite someone new!</h2>
                     </MDBCol>
-                    <MDBCol size="7">
+                    <MDBCol size="5">
                         <img src="section-4-image.jpg" className="img-fluid" id="section-4-img"></img>
                     </MDBCol>
                 </MDBRow>
 
-                <br></br>
-
-                <MDBRow>
+                <MDBRow className="section-margin">
                     <MDBCol size="12">
                         <EventCard title="Bubble Tea at Icon" description="No one has made this event yet...It could be you!" sample={true}/>
                     </MDBCol>
