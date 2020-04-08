70561d9be43f9092bfac6cc60e54e19d5f71e592
thedarrch <dtcheung@edu.uwaterloo.ca>
2020-04-08 16:01:08 -0400

    fix font and sizing

--- src/Components/CreateEventPage.js
+++ src/Components/CreateEventPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBInput, MDBContainer } from 'mdbreact';
+import { MDBInput, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import './styles/CreateEventPage.css';
 import SubmitButton from './SubmitButton';
 import axios from "axios";
@@ -23,59 +23,101 @@ class CreateEventPage extends Component {
                 <div className="app-page-fill"></div>
                 <div className="app-main-section">
                     <MDBContainer id="create-page">
-                        <h1>Make Event</h1>
+                        <div class="h5">Make Event</div>
                         <form id="create-form">
-                            <MDBInput
-                                className="textField"
-                                autoComplete="off"
-                                type="text"
-                                name="eventName"
-                                label="Event Name"
-                                onChange={(e) => this.setState({eventName: e.target.value})}
-                            />
-                            <MDBInput
-                                className="textField"
-                                autoComplete="off"
-                                type="text"
-                                name="exDescription"
-                                label="External Description"
-                                onChange={(e) => this.setState({exDescription: e.target.value})}
-                            />
-                            <MDBInput
-                                className="textField"
-                                autoComplete="off"
-                                type="text"
-                                name="inDescription"
-                                label="Internal Description"
-                                onChange={(e) => this.setState({inDescription: e.target.value})}
-                            />
-                            <MDBInput
-                                className="textField"
-                                autoComplete="off"
-                                type="text"
-                                name="address"
-                                label="Address"
-                                onChange={(e) => this.setState({address: e.target.value})}
-                            />
-                            <MDBInput
-                                className="textField"
-                                autoComplete="off"
-                                type="text"
-                                name="city"
-                                label="City"
-                                onChange={(e) => this.setState({city: e.target.value})}
-                            />
-                            <MDBInput
-                                className="textField"
-                                autoComplete="off"
-                                type="text"
-                                name="link"
-                                label="Cross-post Link"
-                                onChange={(e) => this.setState({link: e.target.value})}
-                            />
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>Event Name:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <MDBInput
+                                    className="textField"
+                                    autoComplete="off"
+                                    type="text"
+                                    name="eventName"
+                                    onChange={(e) => this.setState({eventName: e.target.value})}
+                                    /> 
+                                </MDBCol>   
+                            </MDBRow>
+                            
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>External Description:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <MDBInput
+                                        className="textField"
+                                        autoComplete="off"
+                                        type="text"
+                                        name="exDescription"
+                                        onChange={(e) => this.setState({exDescription: e.target.value})}
+                                    />
+                                </MDBCol>   
+                            </MDBRow>
+                           
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>Internal Description:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <MDBInput
+                                        className="textField"
+                                        autoComplete="off"
+                                        type="text"
+                                        name="inDescription"
+                                        onChange={(e) => this.setState({inDescription: e.target.value})}
+                                    /> 
+                                </MDBCol>   
+                            </MDBRow>
+
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>Address:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <MDBInput
+                                        className="textField"
+                                        autoComplete="off"
+                                        type="text"
+                                        name="address"
+                                        onChange={(e) => this.setState({address: e.target.value})}
+                                    /> 
+                                </MDBCol>   
+                            </MDBRow>
+
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>City:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <MDBInput
+                                        className="textField"
+                                        autoComplete="off"
+                                        type="text"
+                                        name="city"
+                                        onChange={(e) => this.setState({city: e.target.value})}
+                                    /> 
+                                </MDBCol>   
+                            </MDBRow>
+
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>Cross-post Link:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <MDBInput
+                                        className="textField"
+                                        autoComplete="off"
+                                        type="text"
+                                        name="link"
+                                        onChange={(e) => this.setState({link: e.target.value})}
+                                    />
+                                </MDBCol>   
+                            </MDBRow>
+
                             <SubmitButton id="create-event" text="Submit" type={"createEvent"} eventName={this.state.eventName} exDescription={this.state.exDescription} 
                             inDescription={this.state.inDescription} address={this.state.address} city={this.state.city} link={this.state.link}
-                            />
+                            /> 
                         </form>
                     </MDBContainer>
                 </div>
