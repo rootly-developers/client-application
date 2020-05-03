8b6601d0690752d6dd4fc16e2bd97f5bd3d513c6
the.cold.moon <lyliu013@gmail.com>
2020-05-02 22:12:27 -0400

    Add logout button

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -35,6 +35,7 @@ class ProfilePage extends Component {
                             <div className="utilities">
                                 <MDBBtn type="" className="btn editProfile">Edit Profile</MDBBtn>
                                 <MDBBtn type="" className="btn changePassword">Change Password</MDBBtn>
+                                <MDBBtn type="" className="btn logout">logout</MDBBtn>
                             </div>
                         </MDBRow>
                     </div>
