69064d5e29cc2d5accaae254252fda3551caa62f
the.cold.moon <lyliu013@gmail.com>
2020-05-04 17:29:06 -0400

    Move images to public

--- src/images.js
+++ src/images.js
@@ -1,20 +1,20 @@
 const images = {
     avatar: {
-        bunny: require('./images/avatars/bunny.jpg'),
-        chicken: require('./images/avatars/chicken.jpg'),
-        goose: require('./images/avatars/goose.jpg'),
-        octo: require('./images/avatars/octo.jpg'),
-        pebbles: require('./images/avatars/pebbles.jpg'),
-        turtle: require('./images/avatars/turtle.jpg'),
+        bunny: '/images/avatars/bunny.jpg',
+        chicken: '/images/avatars/chicken.jpg',
+        goose: '/images/avatars/goose.jpg',
+        octo: '/images/avatars/octo.jpg',
+        pebbles: '/images/avatars/pebbles.jpg',
+        turtle: '/images/avatars/turtle.jpg',
     },
 
     events: {
-        adventure: require('./images/events/adventure.png'),
-        coffee: require('./images/events/coffee.png'),
-        games: require('./images/events/games.png'),
-        ideas: require('./images/events/ideas.png'),
-        sports: require('./images/events/sports.png'),
-        theatre: require('./images/events/theatre.png')
+        adventure: '/images/events/adventure.png',
+        coffee: '/images/events/coffee.png',
+        games: '/images/events/games.png',
+        ideas: '/images/events/ideas.png',
+        sports: '/images/events/sports.png',
+        theatre: '/images/events/theatre.png'
     }
 };
 
