1957c9e19b1a42c211d80a419c5cf4d24c23f667
thedarrch <dtcheung@edu.uwaterloo.ca>
2020-05-04 02:57:39 -0400

    import/add datepicker for date field

--- src/Components/CreateEventPage.js
+++ src/Components/CreateEventPage.js
@@ -4,6 +4,8 @@ import './styles/CreateEventPage.css';
 import SubmitButton from './SubmitButton';
 import axios from "axios";
 import Select from 'react-select'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const options = [
     { value: 'San Francisco', label: 'San Francisco' },
@@ -21,8 +23,43 @@ class CreateEventPage extends Component {
             address: '',
             city: '',
             region: '',
+            date: new Date(),
             link: ''
         }
+<<<<<<< HEAD
+=======
+        this.handleAPICall = this.handleAPICall.bind(this);
+    }
+
+    handleChange = date => {
+        this.setState({
+          date: date
+        });
+      };
+
+    handleAPICall() {
+        return new Promise((resolve, reject) => {
+            axios.post('http://localhost:8080/events', {
+            eventName: this.state.eventName,
+            exDescription: this.state.exDescription,
+            inDescription: this.state.inDescription,
+            address: this.state.address,
+            city: this.state.city,
+            link: this.state.link
+            // TODO: this should have more data but not sure what to do yet for elements that exist only in mdb pro (select/datepicker/timepicker)
+            })
+            .then((res) => {
+                if (res.status == 200) {
+                    console.log(res);
+                    let redirectPath = "/" + res.eventData;
+                    resolve({redirectPath});
+                }
+                else {
+                    reject("Error");
+                }
+            })
+        })
+>>>>>>> import/add datepicker for date field
     }
 
     render() {
@@ -114,6 +151,18 @@ class CreateEventPage extends Component {
                                         name="city"
                                         onChange={(e) => this.setState({city: e.target.value})}
                                     /> 
+                                </MDBCol>   
+                            </MDBRow>
+
+                            <MDBRow>
+                               <MDBCol size="3">
+                                    <h4>Date:</h4>
+                               </MDBCol>
+                               <MDBCol size="9">
+                                    <DatePicker
+                                        selected={this.state.date}
+                                        onChange={this.handleChange}
+                                    />
                                 </MDBCol>   
                             </MDBRow>
 
